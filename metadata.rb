fd7d42fcbf01d0f4da504beb8e7e3e62362a8645
KernelMadness <kernelmadness@nine9.ru>
2014-04-09 15:52:04 +0400

    fix max client body size

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dsrizaev@gmail.com'
 license          'All rights reserved'
 description      'Installs full-stack rails app server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'apt'
 depends 'bluepill'
